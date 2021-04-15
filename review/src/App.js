e1ef1ff455efb0098cb4493f6b4564421fd4bf9f
Dustin Myers <dustint314@gmail.com>
2021-04-15 12:01:25 -0600

    wip

--- review/src/App.js
+++ review/src/App.js
@@ -63,3 +63,69 @@ const Name = () => {
     </>
   );
 };
+
+// import React, { createContext, useState, useContext } from "react";
+// import brad from "./data";
+
+// // STEP 1 - create a context obj
+// const TitleContext = createContext();
+// console.log(TitleContext);
+
+// const App = () => {
+//   const [title, setTitle] = useState("Hello World");
+//   const [person, setPerson] = useState(brad);
+
+//   // STEP 2 - Provide data to the component tree with
+//   // the Provider component
+//   // { title, person } === { title: title, person: person }
+//   return (
+//     <TitleContext.Provider value={[person, title, setPerson]}>
+//       <Child />
+//     </TitleContext.Provider>
+//   );
+// };
+
+// export default App;
+
+// // intermediary component (Dustin-ism)
+// const Child = () => {
+//   return <Grandchild />;
+// };
+
+// const Grandchild = () => {
+//   // STEP 3 - consume data in a nested component from the
+//   // context Provider
+//   const [, title] = useContext(TitleContext);
+
+//   return (
+//     <>
+//       <h1>{title}</h1>
+//       <Name />
+//       {/* <Address /> */}
+//       {/* <ProfilePic /> */}
+//     </>
+//   );
+// };
+
+// const Name = () => {
+//   const [person, , setPerson] = useContext(TitleContext);
+//   const addYear = () => {
+//     setPerson({
+//       ...person,
+//       dob: {
+//         ...person.dob,
+//         age: person.dob.age + 1,
+//       },
+//     });
+//   };
+//   // consume and display Brad's name data from context
+//   return (
+//     <>
+//       <h3>
+//         {person.name.last}, {person.name.first}
+//       </h3>
+//       <p>Age: {person.dob.age}</p>
+//       <button onClick={() => addYear()}>Happy Birthday!</button>
+//     </>
+//   );
+// };
