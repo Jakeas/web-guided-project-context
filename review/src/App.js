9616275c0141d0e85ae26645ee6b153c2dc6cb2c
Dustin Myers <dustint314@gmail.com>
2021-04-15 10:19:23 -0600

    wip

--- review/src/App.js
+++ review/src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const App = ()=> {
-    return(<h1>Hello World</h1>);
+const App = () => {
+  return <Child />;
 };
 
 export default App;
+
+const Child = () => {
+  return <Grandchild />;
+};
+
+const Grandchild = () => {
+  return <h1>Hello World</h1>;
+};
